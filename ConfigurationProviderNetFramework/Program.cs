89fa2ecb9a3ca73296edee005413c5bcfb75c80a
Shiv Kumar <shiv.kmr003@gmail.com>
2019-08-12 14:57:07 -0400

    Add project files.

---
+++ ConfigurationProviderNetFramework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationProviderNetFramework
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurationProvider = new ConfigurationProvider();
+            Console.WriteLine(configurationProvider.EmailTemplatesPath);
+            Console.WriteLine(configurationProvider.PaymentGatewayServiceUrl);
+            Console.WriteLine(configurationProvider.FiscalYearStart.ToLongDateString());
+            Console.WriteLine(configurationProvider.NotifyOnUpload);
+
+            var dbConnectionInformation = configurationProvider.DbConnectionInformation;
+            Console.WriteLine(dbConnectionInformation.ConnectionStringName);
+            Console.WriteLine(dbConnectionInformation.ConnectionString);
+            Console.WriteLine(dbConnectionInformation.ProviderName);
+        }
+    }
+}
